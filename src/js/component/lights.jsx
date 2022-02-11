0231f084bcd70c7392fa9fbd1e2057cadfd86a1d
Jose <josejesuscedeno@gmail.com>
2022-02-11 19:26:47 +0000

    semicreado cuadros de luz

--- src/js/component/lights.jsx
+++ src/js/component/lights.jsx
@@ -2,10 +2,28 @@ import React, { useState } from "react";
 
 const Lights = () => {
 	return (
-		<div className="container bg">
-			<div className="row">red</div>
-			<div className="row">yellow</div>
-			<div className="row">green</div>
+		<div className="container ">
+			<div className="row col-2 p-3 mb-2 bg-dark text-white h-50">
+				<div className="row bg-danger rounded-circle">
+					<p>p</p>
+					<p>p</p>
+					<p>p</p>
+				</div>
+			</div>
+			<div className="row col-2 p-3 mb-2 bg-dark text-white">
+				<div className="bg-warning rounded-circle">
+					<p>p</p>
+					<p>p</p>
+					<p>p</p>
+				</div>
+			</div>
+			<div className="row col-2 p-3 mb-2 bg-dark text-white ">
+				<div className="bg-success rounded-circle">
+					<p>p</p>
+					<p>p</p>
+					<p>p</p>
+				</div>
+			</div>
 		</div>
 	);
 };

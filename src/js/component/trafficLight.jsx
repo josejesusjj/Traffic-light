8a470c754d3bd3b5204372b353e414b26cd830e0
Jose <josejesuscedeno@gmail.com>
2022-02-12 09:16:36 +0000

    commit

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -2,27 +2,26 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const TrafficLight = (props) => {
+	const [Activating, setActivating] = useState("bg-danger");
+
 	return (
 		<div className="container ">
 			<div className="row col-2 p-3 mb-2 bg-dark text-white">
-				<div className="bg-danger rounded-circle circle">
+				<div className="${Activating} rounded-circle circle">
 					{props.turnOn}
-					<button className="circle rounded-circle bg-danger">
-						click aqui
-					</button>
+					<button className="circle rounded-circle bg-danger"></button>
 				</div>
 			</div>
 			<div className="row col-2 p-3 mb-2 bg-dark text-white">
 				<div className="bg-warning rounded-circle circle">
 					{props.turnOn}
-					<button className="circle rounded-circle bg-danger redon">
-						click aqui
-					</button>
+					<button className="circle rounded-circle bg-warning redon"></button>
 				</div>
 			</div>
 			<div className="row col-2 p-3 mb-2 bg-dark text-white ">
 				<div className="bg-success rounded-circle circle">
 					{props.turnOn}
+					<button className="circle rounded-circle bg-success redon"></button>
 				</div>
 			</div>
 		</div>

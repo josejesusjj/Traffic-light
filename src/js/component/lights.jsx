a7e534bc62869c8d41c745e002820804173d546b
Jose <josejesuscedeno@gmail.com>
2022-02-23 15:55:19 +0000

    casi listo

--- src/js/component/lights.jsx
+++ src/js/component/lights.jsx
@@ -1,7 +1,47 @@
 import React, { useState } from "react";
 
 const Lights = () => {
-	return 1;
+	const [addLight, setAddLight] = useState("");
+	const [selectedColor, setSelectedColor] = useState("green");
+
+	return (
+		<div className="environment">
+			<div className="trafficbar"></div>
+			<div className="trafficlight">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor == "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" +
+						(selectedColor == "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" +
+						(selectedColor == "green" ? " glow" : "")
+					}></div>
+				<div
+					className={addLight}
+					onClick={() => setSelectedColor("purple")}></div>
+			</div>
+			<div>
+				<button
+					onClick={() =>
+						setAddLight(
+							"light purple" +
+								(selectedColor == "purple" ? " glow" : "")
+						)
+					}>
+					want a light extra?
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Lights;
